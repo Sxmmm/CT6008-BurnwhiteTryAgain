8df85b8b776b56547698b125ed536ef02aa91428
tomtoye <toye.tom@gmail.com>
2020-01-31 20:05:44 +0000

    Added base Boss stuff

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,69 @@
+﻿//TOYE
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Boss : MonoBehaviour
+{
+    public float health;
+    public float runSpeed;
+    public float turnSpeed;
+
+
+    [SerializeField] private NavMeshAgent navAgent;
+    [SerializeField] private Animator animator;
+
+
+
+    private enum BossState {
+        Idle,
+        GoHome,
+        Heal,
+        ShootAttack,
+        MeleeAttack,
+
+    }
+
+    private BossState currentState;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void ChooseAbility()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (currentState)
+        {
+            case BossState.Idle:
+
+                break;
+            case BossState.GoHome:
+                break;
+            case BossState.Heal:
+                break;
+            case BossState.ShootAttack:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void SetNavTarget(Vector3 vector3)
+    {
+        navAgent.enabled = true;
+        navAgent.SetDestination(vector3);
+    }
+
+}
---
+++ Assets/Scripts/BossAbility.cs
@@ -0,0 +1,31 @@
+﻿//TOYE
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAbility : MonoBehaviour
+{
+    private Boss boss;
+    public void AbilitySetUp(Boss boss)
+    {
+        this.boss = boss;
+    }
+
+    public void AbilityStart()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void AbilityUpdate()
+    {
+        
+    }
+
+    public void StopAbility()
+    {
+
+    }
+
+}
